e8f4f587116199da2058970c18b5571ec406e850
Michael <michaeljameshamlin@gmail.com>
2022-10-04 14:34:30 -0500

    Made color selection theme functional from the menu item flyout on the main calculator page, need to do the same for the other pages.

--- src/Calculator/AppShell.xaml.cs
+++ src/Calculator/AppShell.xaml.cs
@@ -8,4 +8,29 @@ public partial class AppShell : Shell
 
 		Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
 	}
+
+	private void LightThemeMenuItem_Clicked(object sender, EventArgs e)
+	{
+		Resources["PrimaryColor"] = Colors.White;
+        Resources["PageBackgroundColor"] = Colors.LightGrey;
+
+    }
+
+	private void DarkThemeMenuItem_Clicked(object sender, EventArgs e)
+	{
+        Resources["PrimaryColor"] = Colors.DarkGray;
+        Resources["PageBackgroundColor"] = Colors.Black;
+    }
+
+	private void PinkThemeMenuItem_Clicked(object sender, EventArgs e)
+	{
+        Resources["PrimaryColor"] = Colors.Pink;
+        Resources["PageBackgroundColor"] = Colors.LightPink;
+    }
+
+	private void GreenThemeMenuItem_Clicked(object sender, EventArgs e)
+	{
+        Resources["PrimaryColor"] = Colors.Green;
+        Resources["PageBackgroundColor"] = Colors.DarkGreen;
+    }
 }
---
+++ src/Calculator/Resources/Themes/LightTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace Calculator.Resources.Themes;
+
+public partial class LightTheme : ResourceDictionary
+{
+	public LightTheme()
+	{
+		InitializeComponent();
+	}
+}
