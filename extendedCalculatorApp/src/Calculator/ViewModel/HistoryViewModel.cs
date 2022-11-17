8c524df9ecbe8ebfbe9fd11d45304f048dd0655b
Michael Hogan <mjhogan38@gmail.com>
2022-11-16 19:16:09 -0600

    Added History ViewModel for the calculation history page.

---
+++ extendedCalculatorApp/src/Calculator/ViewModel/HistoryViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Calculator.ViewModel
+{
+    public partial class HistoryViewModel : ObservableObject
+    {
+        public HistoryViewModel()
+        {
+            PreviousCalculations = new();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> previousCalculations;
+
+        [ObservableProperty]
+        string lastCalculation;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(LastCalculation))
+                return;
+            PreviousCalculations.Add(LastCalculation);
+        }
+
+        [RelayCommand]
+        void ClearHistory()
+        {
+            PreviousCalculations?.Clear();
+        }
+
+    }
+}
